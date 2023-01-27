cb25020fcdd6c84f3f5122d55635cfb3f76a8c1d
Nguyen Anh Tuan <nguyenanhtuan19122002@gmail.com>
2023-01-27 11:19:35 +0700

    Add and update something

--- App.jsx
+++ App.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
-import { ExploreScreen } from 'screens'
-
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <ExploreScreen />
     </View>
   )
 }
