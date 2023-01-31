47226467396422c4728978ab78b4e086859c065a
FromSunNews <phuongtu590@gmail.com>
2023-01-31 10:13:23 +0700

    Setup redux-toolkit for DongNaiTravelApp

--- App.jsx
+++ App.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
+import { store, persistor } from 'redux/store'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+          <Text>Hello</Text>
+        </View>
+      </PersistGate>
+    </Provider>
   )
 }
 
---
+++ app/utilities/constants.js
@@ -0,0 +1,3 @@
+// Phuong: Created on 31/1/2023
+// Phuong: This is the api web for DongNaiAppTravel
+export const API_ROOT = 'http://localhost:8017'
