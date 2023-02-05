eb6935536460ee2721b851b5b52f0ab144d226cd
FromSunNews <phuongtu590@gmail.com>
2023-02-05 07:55:02 +0700

    Update api root status

--- app/utilities/constants.js
+++ app/utilities/constants.js
@@ -1,3 +1,8 @@
 // Phuong: Created on 31/1/2023
+// Phuong: Muon test api thi xem ipv4 trong may tinh => cmd => ipconfig => ipv4
+// Phuong: doi lai ipv4 cua minh
+const ip_v4 = '192.168.1.10'
+const API_PORT = '7500'
+
 // Phuong: This is the api web for DongNaiAppTravel
-export const API_ROOT = 'http://192.168.1.10:7500'
+export const API_ROOT = `http://${ip_v4}:${API_PORT}`
