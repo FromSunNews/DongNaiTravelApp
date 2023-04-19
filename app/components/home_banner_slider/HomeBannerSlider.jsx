14e2b06e7b3b8d8859b78cf3984d001a80c779e5
Thái Anh Đức <kinaobancan231@gmail.com>
2023-04-19 22:19:12 +0700

    fix language for banner

--- app/components/home_banner_slider/HomeBannerSlider.jsx
+++ app/components/home_banner_slider/HomeBannerSlider.jsx
@@ -1,73 +1,73 @@
 import { app_c } from "globals/styles";
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import { View, Image, StatusBar, Dimensions } from "react-native";
 import Swiper from "react-native-swiper";
+import { languageData } from "../../utilities/language";
+import { useSelector } from "react-redux";
 const { width, height } = Dimensions.get("window");
+import { selectCurrentLanguage } from 'redux/language/LanguageSlice'
 
 
+const HomeBannerSlider = ()=>{
 
-export default class extends Component {
-  render() {
+  const langCode = useSelector(selectCurrentLanguage).languageCode // vi or en 
+  const [ banners, setBanner ] =  useState([])
 
-    const banners = [
-      {
-        uri: "https://res.cloudinary.com/dbtb0sjby/image/upload/v1681802401/cattien_vi-min_hpwzfv.png",
-      },
-      {
-        uri: "https://res.cloudinary.com/dbtb0sjby/image/upload/v1681802378/z4274604337154_4c684a28b00a8fa4bc273dee07d24440-min_h6ycze.jpg",
-      },
-      {
-        uri: "https://res.cloudinary.com/dbtb0sjby/image/upload/v1681802378/z4274603026425_93c337f3c7836a85472e5857ff463fff-min_ajssaq.jpg",
-      },
-      {
-        uri: "https://res.cloudinary.com/dbtb0sjby/image/upload/v1681802399/suoimo_en-min_uk48qt.png",
-      },
+  console.log(langCode)
+
+  useEffect(()=>{
+    if(langCode === 'vi')
+    {
+      setBanner(bannersVi)
+    }
+    if(langCode === 'en')
+    {
+      setBanner(bannersEn)
+    }
+  })
+  console.log(banners)
+  
+  return (
+    <View style={styles.container}>
       
-      {
-        uri: "https://res.cloudinary.com/dbtb0sjby/image/upload/v1681802378/z4274604000111_d25b95af67a4619a2a50324240bf9abd-min_l4uaff.jpg",
-      },
-      
-    ];
-    return (
-      <View style={styles.container}>
-        <StatusBar barStyle="light-content" />
-        <Swiper
-          style={styles.wrapper}
-          autoplay
-          loop
-          dot={
-            <View
-              style={styles.dot}
-            />
-          }
-          activeDot={
-            <View
-              style={styles.active_dot}
-            />
-          }
-          paginationStyle={{
-            bottom: 10,
-          }}
-        >
-          {
-            banners.map((banner,index)=>{
-              return(
-              <View key={index} style={styles.slide}>
-                <Image
-                  style={styles.image}
-                  source={{uri : banner.uri}}
-                  resizeMode="cover"
-                />
-              </View>
-              )
-            })
-          }
-        </Swiper>
-      </View>
-    );
-  }
+      <Swiper
+        style={styles.wrapper}
+        autoplay
+        loop
+        dot={
+          <View
+            style={styles.dot}
+          />
+        }
+        activeDot={
+          <View
+            style={styles.active_dot}
+          />
+        }
+        paginationStyle={{
+          bottom: 10,
+        }}
+      >
+        {
+          banners.map((banner,index)=>{
+            return(
+            <View key={index} style={styles.slide}>
+              <Image
+                style={styles.image}
+                source={{uri : banner.uri}}
+                resizeMode="cover"
+              />
+            </View>
+            )
+          })
+        }
+      </Swiper>
+    </View>
+  );
 }
 
+export default HomeBannerSlider
+    
 const styles = {
   wrapper: {
     // backgroundColor: '#f00'
@@ -108,3 +108,28 @@ const styles = {
     marginRight: 3,
   }
 };
+
+const bannersVi = [
+  {
+    uri: "https://res.cloudinary.com/dbtb0sjby/image/upload/v1681829574/banners/z4274604337154_4c684a28b00a8fa4bc273dee07d24440-min_rqpmcq.jpg",
+  },
+  {
+    uri: "https://res.cloudinary.com/dbtb0sjby/image/upload/v1681829447/banners/chuachan_vi_hs3b4n.png",
+  },
+  {
+    uri: "https://res.cloudinary.com/dbtb0sjby/image/upload/v1681829232/banners/dahan_vi_kpo4ml.png",
+  },
+
+];
+const bannersEn = [
+  {
+    uri: "https://res.cloudinary.com/dbtb0sjby/image/upload/v1681829574/banners/z4274604000111_d25b95af67a4619a2a50324240bf9abd-min_zmekol.jpg",
+  },
+  {
+    uri: "https://res.cloudinary.com/dbtb0sjby/image/upload/v1681829446/banners/chuachan_en_vxdup4.png",
+  },
+  {
+    uri: "https://res.cloudinary.com/dbtb0sjby/image/upload/v1681829228/banners/dahan_en_cy8rx6.png",
+  },
+  
+];
