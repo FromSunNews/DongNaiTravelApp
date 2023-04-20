ea4047c41d8318697d9f2227377c12b88cbba7bc
Thái Anh Đức <kinaobancan231@gmail.com>
2023-04-20 08:29:21 +0700

    update

--- app/utilities/constants.js
+++ app/utilities/constants.js
@@ -3,7 +3,7 @@
 // Phuong: Muon test api thi xem ipv4 trong may tinh => cmd => ipconfig => ipv4
 // Phuong: doi lai ipv4 cua minh
 
-const ip_v4 = '192.168.43.83'
+const ip_v4 = '172.20.10.3'
 
 const API_PORT = '7500'
 
