0145db6d803c63312a055904de9de52b4b5fec6a
Từ Nhật Phương <111409554+FromSunNews@users.noreply.github.com>
2023-05-07 09:44:31 +0700

    Update constants.js

--- app/utilities/constants.js
+++ app/utilities/constants.js
@@ -1,11 +1,8 @@
 
 // Phuong: Created on 31/1/2023
-// Phuong: Muon test api thi xem ipv4 trong may tinh => cmd => ipconfig => ipv4
-// Phuong: doi lai ipv4 cua minh
 
-const ip_v4 = '192.168.1.6'
-
-const API_PORT = '7500'
+// Phuong: This is the api web for DongNaiAppTravel
+export const API_ROOT = 'https://dong-nai-travel-api.onrender.com'
 
 // Phuong: This is the api web for DongNaiAppTravel
 export const API_ROOT = `http://${ip_v4}:${API_PORT}`
